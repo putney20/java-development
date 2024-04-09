cdaa914d3ed221be5278df98faea551abd02bad6
Charles Putney <rputney8@gmail.com>
2024-04-09 11:07:11 -0400

    Completed Shopping List App

---
+++ workbook1/first-maven-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class HelloWorld {
+    //main method - entry point in the JVM
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
